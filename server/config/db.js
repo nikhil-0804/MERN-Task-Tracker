1deeb3c535b6c0b02d76c059d01fe3761946e551
Nikhil Rana <nikhilrana6191@gmail.com>
2026-06-28 21:39:24 +0530

    Fix CORS

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("✅ MongoDB Connected Successfully");
     } catch (error) {
         console.log("❌ MongoDB Connection Error:", error.message);
